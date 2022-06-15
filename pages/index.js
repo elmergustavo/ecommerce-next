28b87f9320b01fe6f84d30473b5b583706d5a3a3
elmergustavo <elmergustavo79@gmail.com>
2022-06-15 00:32:18 -0600

    config css and proyect general

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const index = () => {
   return (
-    <div>index</div>
+    <>
+      HeroBanner
+
+      <div className="products-heading">
+        <h2>Beset selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+    </>
   )
 }
 
