05f26abb2118633b97515cb58ba322c9720542a5
elmergustavo <elmergustavo79@gmail.com>
2022-06-15 20:18:06 -0600

    sanity configuration

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,33 @@
-import React from 'react'
+import React from "react";
+import { client } from "../lib/client";
+import { Product, FooterBanner, HeroBanner } from "../components";
 
-const index = () => {
+const index = ({products, bannerData}) => {
   return (
     <>
-      HeroBanner
-
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
       <div className="products-heading">
         <h2>Beset selling Products</h2>
-        <p>Speakers of many variations</p>
+        <p>Speakers of many variations passages</p>
+      </div>
+
+      <div className="products-container">
+          {products?.map((product) => product.name)}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default index
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
+
+export default index;
