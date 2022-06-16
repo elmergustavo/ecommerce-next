64d704362fc24b3541b0e500d0fe2834fd8f4900
elmergustavo <elmergustavo79@gmail.com>
2022-06-15 21:12:29 -0600

    Footer layout

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,21 @@ import React from "react";
 import { client } from "../lib/client";
 import { Product, FooterBanner, HeroBanner } from "../components";
 
-const index = ({products, bannerData}) => {
+const index = ({ products, bannerData }) => {
   return (
-    <>
-      <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
-      <div className="products-heading">
-        <h2>Beset selling Products</h2>
-        <p>Speakers of many variations passages</p>
-      </div>
+    <div>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
+    <div className="products-heading">
+      <h2>Best Seller Products</h2>
+      <p>speaker There are many variations passages</p>
+    </div>
 
-      <div className="products-container">
-          {products?.map((product) => product.name)}
-      </div>
-    </>
+    <div className="products-container">
+      {products?.map((product) => <Product key={product._id} product={product} />)}
+    </div>
+
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />
+  </div>
   );
 };
 
